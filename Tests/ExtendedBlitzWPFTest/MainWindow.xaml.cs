05a0a977eaf0ca80c67472b7b589136990896faf
hikkathon <hikkathon@gmail.com>
2022-02-11 23:06:37 +0700

    Инфраструктура папок приложения

--- Tests/ExtendedBlitzWPFTest/MainWindow.xaml.cs
+++ Tests/ExtendedBlitzWPFTest/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,9 +10,6 @@ using System.Windows.Shapes;
 
 namespace ExtendedBlitzWPFTest
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
