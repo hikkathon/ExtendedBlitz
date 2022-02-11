5074da6a1d5480dee566cee9c3beb21204ad532b
hikkathon <hikkathon@gmail.com>
2022-02-11 23:47:54 +0700

    Тестирование работы модели-представления главного окна

---
+++ ExtendedBlitz/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExtendedBlitz.ViewModels.Base;
+
+namespace ExtendedBlitz.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title = "Extended Blitz";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
