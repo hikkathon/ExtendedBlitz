380c9f0edcd7c1f728eef3001f8451449fb2c361
hikkathon <hikkathon@gmail.com>
2022-02-12 00:13:27 +0700

    Главный класс команды

---
+++ ExtendedBlitz/Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ExtendedBlitz.Infrastructure.Commands.Base;
+
+namespace ExtendedBlitz.Infrastructure.Commands
+{
+    internal class RelayCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> Ececute, Func<object, bool> CanExecute = null)
+        {
+            _execute= Ececute ?? throw new ArgumentNullException(nameof(Ececute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
