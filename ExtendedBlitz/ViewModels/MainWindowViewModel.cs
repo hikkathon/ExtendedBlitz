44f9d79d76653217bad49095398a1d94a0129a79
hikkathon <hikkathon@gmail.com>
2022-02-13 00:48:23 +0700

    Реализовал WindowState команды

--- ExtendedBlitz/ViewModels/MainWindowViewModel.cs
+++ ExtendedBlitz/ViewModels/MainWindowViewModel.cs
@@ -33,9 +33,77 @@ namespace ExtendedBlitz.ViewModels
 
         #endregion
 
+        #region WindowState - Состояние окна
+
+        private WindowState _curWindowState = WindowState.Normal;
+
+        /// <summary> Свернуть окно программы </summary>
+        public WindowState CurWindowState
+        {
+            get => _curWindowState;
+            set => Set(ref _curWindowState, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #region MinimizedWindowCommand
+
+        public ICommand MinimizedWindowCommand { get; }
+        private bool CanMinimizedWindowCommandExecuted(object p) => true;
+        private void OnMinimizedWindowCommandExecuted(object p)
+        {
+            CurWindowState = WindowState.Minimized;
+        }
+
+        #endregion
+
+        #region MaximizedWindowCommand
+
+        public ICommand MaximizedWindowCommand { get; }
+        private bool CanMaximizedWindowCommandExecuted(object p) => true;
+        private void OnMaximizedWindowCommandExecuted(object p)
+        {
+            CurWindowState = WindowState.Maximized;
+        }
+
+        #endregion
+
+        #region NormalWindowCommand
+
+        public ICommand NormalWindowCommand { get; }
+        private bool CanNormalWindowCommandExecuted(object p) => true;
+        private void OnNormalWindowCommandExecuted(object p)
+        {
+            CurWindowState = WindowState.Normal;
+        }
+
+        #endregion
+
+        #endregion
+
         public MainWindowViewModel()
         {
+            #region Команды
 
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+            MinimizedWindowCommand = new RelayCommand(OnMinimizedWindowCommandExecuted, CanMinimizedWindowCommandExecuted);
+            MaximizedWindowCommand = new RelayCommand(OnMaximizedWindowCommandExecuted, CanMaximizedWindowCommandExecuted);
+            NormalWindowCommand = new RelayCommand(OnNormalWindowCommandExecuted, CanNormalWindowCommandExecuted);
+
+            #endregion
         }
     }
 }
