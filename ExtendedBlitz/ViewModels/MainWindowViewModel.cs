a9ad3206b36b866062d5ea274ec20e089d5dbd3d
hikkathon <hikkathon@gmail.com>
2022-02-12 19:59:06 +0700

    Тестовая разметка окна

--- ExtendedBlitz/ViewModels/MainWindowViewModel.cs
+++ ExtendedBlitz/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Windows.Input;
 using ExtendedBlitz.ViewModels.Base;
+using ExtendedBlitz.Infrastructure.Commands;
+using System.Windows;
 
 namespace ExtendedBlitz.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        #region Заголовок окна
+
         private string _title = "Extended Blitz";
 
         /// <summary>Заголовок окна</summary>
@@ -12,6 +17,34 @@ namespace ExtendedBlitz.ViewModels
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+
+            #endregion
         }
     }
 }
