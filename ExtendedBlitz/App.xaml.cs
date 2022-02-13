be598b04b67bd9d69a449d4f2f99499351323f4d
hikkathon <hikkathon@gmail.com>
2022-02-13 20:07:37 +0700

    Тест DataService

--- ExtendedBlitz/App.xaml.cs
+++ ExtendedBlitz/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ExtendedBlitz.Services;
+using System.Net.Http;
 using System.Windows;
 
 namespace ExtendedBlitz
@@ -13,5 +9,20 @@ namespace ExtendedBlitz
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+            string application_id = "07ac358d831595916aca265c2f14750c";
+            string account_id = "71941826";
+            string region = "ru";
+
+            using (var client = new HttpClient())
+            {
+                var service_test = new DataService(client, application_id, account_id, region);
+                var player = service_test.GetData();
+            }
+        }
     }
 }
---
+++ ExtendedBlitz/Services/DataService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System;
+using System.Net.Http;
+using ExtendedBlitz.Models.WoTBlitz.Personal_data;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace ExtendedBlitz.Services
+{
+    internal class DataService
+    {
+        string application_id;
+        string account_id;
+        string region;
+        HttpClient client;
+
+        public DataService(HttpClient client, string application_id, string account_id, string region)
+        {
+            this.client = client;
+            this.application_id = application_id;
+            this.account_id = account_id;
+            this.region = region;
+        }
+
+        private async Task<string> GetDataStream()
+        {
+            var response = await client.GetAsync($"https://api.wotblitz.ru/wotb/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.rating&language={region}");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Player GetData()
+        {
+            var responseBody = (SynchronizationContext.Current is null ? GetDataStream() : Task.Run(GetDataStream)).Result;
+
+            string substring = account_id.ToString();
+            int index = responseBody.IndexOf(substring);
+            string json = responseBody.Remove(index, substring.Length).Insert(index, "account");
+
+            return JsonConvert.DeserializeObject<Player>(json);
+        }
+    }
+}
