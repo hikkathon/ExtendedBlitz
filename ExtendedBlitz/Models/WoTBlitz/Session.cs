640b013a06305ea9af0d000ff97df4f1ead15cd3
hikkathon <hikkathon@gmail.com>
2022-02-21 01:30:51 +0700

    Тест триггеров в UI разметке

---
+++ ExtendedBlitz/Models/WoTBlitz/Session.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtendedBlitz.Models.WoTBlitz
+{
+    internal class Battle
+    {
+        public int id { get; set; }
+        public string status { get; set; } // победа/поражение
+        public int spotted { get; set; }
+        public int max_frags_tank_id { get; set; }
+        public int hits { get; set; }
+        public int frags { get; set; }
+        public int max_xp { get; set; }
+        public int max_xp_tank_id { get; set; }
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public int capture_points { get; set; }
+        public int battles { get; set; }
+        public int damage_dealt { get; set; }
+        public int damage_received { get; set; }
+        public int max_frags { get; set; }
+        public int shots { get; set; }
+        public int frags8p { get; set; }
+        public int xp { get; set; }
+        public int win_and_survived { get; set; }
+        public int survived_battles { get; set; }
+        public int dropped_capture_points { get; set; }
+    }
+
+    internal class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeSession { get; set; }
+        public ICollection<Battle> Battles { get; set; }
+    }
+}
--- ExtendedBlitz/ViewModels/MainWindowViewModel.cs
+++ ExtendedBlitz/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using ExtendedBlitz.Infrastructure.Commands;
+using System.Collections.ObjectModel;
 using ExtendedBlitz.ViewModels.Base;
+using ExtendedBlitz.Models.WoTBlitz;
 using ExtendedBlitz.Services;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Threading;
 using System.Net.Http;
 using System.Windows;
+using System.Linq;
+using System;
 
 namespace ExtendedBlitz.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        /* ---------------------------------------------------------------------------------------------------- */
+
+        public ObservableCollection<Session> Sessions {get;}
+
         #region Заголовок окна
 
         private string _title = "Extended Blitz";
@@ -49,6 +57,15 @@ namespace ExtendedBlitz.ViewModels
         }
 
         #endregion
+
+        #region Selected battle
+
+        private Session _selectSession;
+        public Session SelectSession { get => _selectSession; set => Set(ref _selectSession, value); }
+
+        #endregion
+
+        /* ---------------------------------------------------------------------------------------------------- */
 
         #region Команды
 
@@ -123,6 +140,8 @@ namespace ExtendedBlitz.ViewModels
 
         #endregion
 
+        /* ---------------------------------------------------------------------------------------------------- */
+
         public MainWindowViewModel()
         {
             #region Команды
@@ -133,6 +152,43 @@ namespace ExtendedBlitz.ViewModels
             NormalWindowCommand     = new RelayCommand(OnNormalWindowCommandExecuted, CanNormalWindowCommandExecuted);
 
             #endregion
+
+            Random rnd = new Random();
+
+            var battles = Enumerable.Range(1, 20).Select(i => new Battle
+            {
+                id = i,
+                status = rnd.Next(-1,1) > rnd.Next(-1, 1) ? "Победа" : "Поражение",
+                spotted = rnd.Next(1, 20),
+                max_frags_tank_id = rnd.Next(1, 20),
+                hits = rnd.Next(1, 20),
+                frags = rnd.Next(1, 20),
+                max_xp = rnd.Next(1, 20),
+                max_xp_tank_id = rnd.Next(1, 20),
+                wins = rnd.Next(1, 20),
+                losses = rnd.Next(1, 20),
+                capture_points = rnd.Next(1, 20),
+                battles = rnd.Next(1, 20),
+                damage_dealt = rnd.Next(1, 20),
+                damage_received = rnd.Next(1, 20),
+                max_frags = rnd.Next(1, 20),
+                shots = rnd.Next(1, 20),
+                frags8p = rnd.Next(1, 20),
+                xp = rnd.Next(1, 20),
+                win_and_survived = rnd.Next(1, 20),
+                survived_battles = rnd.Next(1, 20),
+                dropped_capture_points = rnd.Next(1, 20),
+            });
+
+            var sessions = Enumerable.Range(1, 10).Select(i => new Session
+            {
+                Id = i,
+                Name = $"Сессия {i}",
+                TimeSession = DateTime.Now,
+                Battles = new ObservableCollection<Battle>(battles)
+            });
+
+            Sessions = new ObservableCollection<Session>(sessions);
         }
     }
 }
